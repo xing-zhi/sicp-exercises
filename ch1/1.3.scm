72131b84b71d9f3c8a94689a26a5dedcfe325e98
xing-zhi <xing_zhi@126.com>
2016-06-01 11:42:25 +0800

    exercise 1.3

---
+++ ch1/1.3.scm
@@ -0,0 +1,8 @@
+(define (square x)
+  (* x x))
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(define (sum-of-the-squares-of-the-two-larger x y z)
+  (if (> x y)
+      (if (> y z) (sum-of-squares x y) (sum-of-squares x z))
+      (if (> x z) (sum-of-squares x y) (sum-of-squares y z))))
