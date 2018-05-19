da8021705e78ef09f1815ba970475940bec81942
ranwu <xing_zhi@126.com>
2018-05-19 16:21:53 +0800

    2.26

---
+++ ch2/2.26.scm
@@ -0,0 +1,16 @@
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y)
+;;; => (1 2 3 4 5 6)
+
+(cons x y)
+;;; => ((1 2 3) 4 5 6)
+
+(list x y)
+;;; => ((1 2 3) (4 5 6))
