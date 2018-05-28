0c6643904c207750a0acade7b1b5610165ad21ee
ranwu <xing_zhi@126.com>
2018-05-28 14:10:28 +0800

    1.39

---
+++ ch1/1.39.scm
@@ -0,0 +1,15 @@
+(define (cont-frac n d k)
+  (define (iter result i)
+    (if (= i 0)
+        result
+        (iter (/ (n i) (+ (d i) result)) (- i 1))))
+  (iter (/ (n k) (d k)) (- k 1)))
+
+(define (tan-cf x k)
+  (cont-frac (lambda (i)
+               (if (= i 1)
+                   x
+                   (- (square x))))
+             (lambda (i)
+               (- (* 2 i) 1.0))
+             k))
